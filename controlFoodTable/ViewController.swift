7795d2f389ab4a1792792048d8670babddbdb752
bushair <bushairkm007@gmail.com>
2019-10-30 13:50:27 -0400

    add table view

--- controlFoodTable/ViewController.swift
+++ controlFoodTable/ViewController.swift
@@ -8,13 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    var foods:[String]?
+    var calories:[Int]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        foods = ["Apple","Banana","Burger","Fries","Orange","Pizza"]
+        calories = [50 , 60, 900, 600, 30, 700]
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return foods?.count ?? 0
     }
 
-
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard foods != nil else {return UITableViewCell()}
+       let cell = UITableViewCell(style: .value1, reuseIdentifier: " ")
+        let foodName = foods![indexPath.row]
+        cell.textLabel?.text = foodName
+        cell.imageView?.image = UIImage(named: foodName)
+        cell.detailTextLabel?.text = "calories: \(calories![indexPath.row])"
+        return cell
+        
+    }
 }
 
