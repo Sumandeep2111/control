fc4867bb0994c7c3d96c4da7aa5aed7e2dae19c9
bushair <bushairkm007@gmail.com>
2019-10-30 15:08:39 -0400

    add new table view

--- controlFoodTable/ViewController.swift
+++ controlFoodTable/ViewController.swift
@@ -13,11 +13,16 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     var foods:[String]?
     var calories:[Int]?
     
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
         foods = ["Apple","Banana","Burger","Fries","Orange","Pizza"]
         calories = [50 , 60, 900, 600, 30, 700]
+        tableView.register(foodTableViewCell.self, forCellReuseIdentifier: "food cell")
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -26,11 +31,17 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard foods != nil else {return UITableViewCell()}
-       let cell = UITableViewCell(style: .value1, reuseIdentifier: " ")
+//       let cell = UITableViewCell(style: .value1, reuseIdentifier: " ")
+//        let foodName = foods![indexPath.row]
+//        cell.textLabel?.text = foodName
+//        cell.imageView?.image = UIImage(named: foodName)
+//        cell.detailTextLabel?.text = "calories: \(calories![indexPath.row])"
+//        return cell
         let foodName = foods![indexPath.row]
-        cell.textLabel?.text = foodName
-        cell.imageView?.image = UIImage(named: foodName)
-        cell.detailTextLabel?.text = "calories: \(calories![indexPath.row])"
+        let foodCalory = calories![indexPath.row]
+        // as use for unwrapping
+        let cell = tableView.dequeueReusableCell(withIdentifier: "food cell") as! foodTableViewCell
+        cell.setName(name: foodName, calories: foodCalory, image: foodName)
         return cell
         
     }
--- controlFoodTable/foodTableViewCell.swift
+++ controlFoodTable/foodTableViewCell.swift
@@ -9,16 +9,41 @@
 import UIKit
 
 class foodTableViewCell: UITableViewCell {
+//
+//    override func awakeFromNib() {
+//        super.awakeFromNib()
+//        // Initialization code
+//    }
+//
+//    override func setSelected(_ selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//
+//        // Configure the view for the selected state
+//    }
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    var nameLabel:UILabel?
+    var  caloriesLabel: UILabel?
+    var iconFoodImage:UIImageView?
+    
+//    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+//        super.init(style: style, reuseIdentifier: reuseIdentifier)
+//    }
+//    //subclass init is required to give here with super class init // with any ios class
+//    required init?(coder aDecoder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+    func setName(name: String,calories:Int,image:String) {
+        nameLabel = UILabel(frame: CGRect(x: 10, y: 15, width: 140, height: 40))
+        nameLabel?.text = name
+        caloriesLabel = UILabel(frame: CGRect(x: 160, y: 15, width: 140, height: 40))
+        caloriesLabel?.text = "\(calories) cals"
+        iconFoodImage = UIImageView(image:UIImage( named: image))
+        iconFoodImage?.frame = CGRect(x: 310, y: 6, width: 40, height: 40)
+        //pollymorphism
+        self.contentView.addSubview(nameLabel!)
+        self.contentView.addSubview(caloriesLabel!)
+        self.contentView.addSubview(iconFoodImage!)
+        
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
+    
 }
